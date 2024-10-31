444c0f4be5a0a0093b25e00189fe8100f4787a62
Alexander Ek <djalekzse@gmail.com>
2024-10-31 19:21:41 +1100

    clean up

--- margin-audit.py
+++ margin-audit.py
@@ -17,6 +17,8 @@ erates = [0.0, 0.005, 0.01, 0.02, 0.03, 0.04, 0.05, 0.075, 0.1, 0.15, 0.2, 0.3, 
 def main(error_rate=0.03, margin=0.03, size=50_000):
     # Create CVR data file
     n_mismatch = int(size * error_rate)
+    assert n_mismatch / size == error_rate, "Given margin pct not attainable"  # Ensure there is no rounding error
+
     cvr_input = []
     for i in range(size - n_mismatch):
         cvr_input.append({"id": "match"+str(i+1), "votes": {"1": {"match": 1, "mismatch": 0}}})
@@ -57,14 +59,6 @@ def main(error_rate=0.03, margin=0.03, size=50_000):
 
     # Calculate margins for each assertion.
     min_margin = Assertion.set_all_margins_from_cvrs(audit, contests, cvr_list)
-    #
-    # # Some output.
-    # print(f"Smallest margin in contest: {min_margin}")
-    # print("Assorters and margins:")
-    Contest.print_margins(contests)
-    # print(audit.find_sample_size(contests, cvrs=cvr_list))
-    #prng = SHA256(1)
-    #CVR.assign_sample_nums(cvr_list, prng)
 
     # Calculate all of the p-values.
     pvalue_histories_array = calc_pvalues_all_orderings(contests, cvr_input)
@@ -93,16 +87,6 @@ def main(error_rate=0.03, margin=0.03, size=50_000):
 
 # =============================================================================
 # Define functions.
-
-# Shuffle raw RAIRE-formatted CVRs according to a given ordering.
-#
-# Need to concatenate [0, 1] at the start, to avoid shuffling the file headers.
-# The other indices need to be translated accordingly (by +1).
-def shuffle(cvrs, ordering):
-    neworder = np.concatenate(([0, 1], ordering + 1))
-    cvrs_shuffled = [cvrs[i] for i in neworder]
-    return cvrs_shuffled
-
 
 # Extract a 'merged' p-value history, combined across all assertions.
 #
@@ -152,10 +136,10 @@ def calc_pvalues_all_orderings(contests, cvr_input, n_orderings=1000):
 
 
 if __name__ == "__main__":
-    counter = 0  # 1-13
+    counter = 0  # array size on SLURM cluster == 1-13
     for erate in erates:
         counter += 1
-        if counter != int(os.environ['SLURM_ARRAY_TASK_ID']): continue
+        if counter != int(os.environ['SLURM_ARRAY_TASK_ID']): continue  # parallelise on SLURM cluster
         print("pop_size, error_rate, margin, assertion_margin, sample_size_10pct, sample_size_5pct, sample_size_1pct, "
           "certified_10pct, certified_5pct, certified_1pct")
         for margin in margins:
